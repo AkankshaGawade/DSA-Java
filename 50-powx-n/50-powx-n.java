f5e48e2a976680b7f2eac43373a764cee439699b
Akanksha Gawade <87490936+AkankshaGawade@users.noreply.github.com>
2022-01-25 20:06:06 +0530

    Time: 1 ms (56.25%), Space: 39.2 MB (22.97%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,6 +1,7 @@
 class Solution {
-  public double myPow(double x, int n) {
-        if(n==0)
+    public double myPow(double x, int n)
+    {
+         if(n==0)
             return 1;
         if(n<0 && n>Integer.MIN_VALUE){
             return myPow(1/x,n*-1);
