aec261f6d15fb4ff667996fa5f259f3f928f8335
Akanksha Gawade <87490936+AkankshaGawade@users.noreply.github.com>
2022-02-02 13:36:31 +0530

    Time: 85 ms (45.28%), Space: 41.6 MB (11.46%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -0,0 +1,21 @@
+class Solution {
+    int count=0;
+    public int countArrangement(int n) {
+         boolean[] visited = new boolean[n + 1];
+          helper(n, 1, visited);
+          return count;
+    }
+    private void helper(int n, int start, boolean[] visited) {
+         if (start > n) {
+            count++;
+             return;
+         }
+         for (int i = 1; i <= n; i++) {
+             if (!visited[i] && (start % i == 0 || i % start == 0)) {
+                 visited[i] = true;
+                 helper(n, start + 1, visited);
+                 visited[i] = false;
+             }
+         }
+     }
+}
